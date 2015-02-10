e0b6240f779816835a84584999ad410983f0905e
Vignesh <Vignesh@Vignesh-PC>
2015-02-10 16:51:57 -0500

    Version 2.1.2

--- src/org/usfirst/frc/team3331/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team3331/robot/subsystems/DriveSubsystem.java
@@ -3,16 +3,20 @@ package org.usfirst.frc.team3331.robot.subsystems;
 import org.usfirst.frc.team3331.robot.RobotMap;
 import org.usfirst.frc.team3331.robot.commands.teleopDriveCommand;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class DriveSubsystem extends Subsystem {
-    
+	
     public void initDefaultCommand() {
         setDefaultCommand(new teleopDriveCommand());
     }
     
     public void init() {
     	RobotMap.drivetrain.tankDrive(0, 0);
+    	RobotMap.drivetrain.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+    	RobotMap.drivetrain.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
     }
     
     public void autoDrive_Cartesian(double x, double y, double rotation) {
@@ -25,16 +29,15 @@ public class DriveSubsystem extends Subsystem {
     
     public void teleopDrive() {
     	double axes[] = normaliseAxes();
-    	RobotMap.drivetrain.mecanumDrive_Cartesian(axes[0], axes[1], axes[2], 0);
+    	RobotMap.drivetrain.mecanumDrive_Cartesian(axes[0] * 0.6, axes[1] * 0.9, axes[2] * 0.7, 0);
     	// X, Y, rotation, gyroAngle
     }
     
     private double[] normaliseAxes() {
     	double axes[] = new double[3];
-    	
     	double x = RobotMap.gamepad.getRawAxis(RobotMap.leftStickX), y = RobotMap.gamepad.getRawAxis(RobotMap.leftStickY);
     	double rotation = RobotMap.gamepad.getRawAxis(RobotMap.rightStickX);
-    	
+        
     	axes[0] = Math.copySign(Math.pow(x, 2), x);
     	axes[1] = Math.copySign(Math.pow(y, 2), y);
     	axes[2] = Math.copySign(Math.pow(rotation,  2),  rotation); 
